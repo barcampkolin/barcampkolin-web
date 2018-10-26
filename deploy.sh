2cbdbabdc63bad767f0ee59594d448ed24c9ecd4
Jakub Bouček <pan@jakubboucek.cz>
2018-10-26 12:22:01 +0200

    Deploy commit - update

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,18 @@ echo "Uploading data to SSH…"
 rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}${LOCAL_DIR}/" "${SERVER_NAME}:$REMOTE_DIR/"
 
 echo "Replace file permissions…"
-$SSH sudo fixwww $REMOTE_DIR
+${SSH} sudo fixwww ${REMOTE_DIR}
 
 echo "Remove temporary files…"
-$SSH find $REMOTE_DIR/temp -mindepth 2 -type f -delete
+${SSH} find ${REMOTE_DIR}/temp -mindepth 2 -type f -delete
 
 echo -n "Remove nette email-sent marker… "
-ssh sbl /bin/bash << EOF
-    if [ -f ${REMOTE_DIR}/log/email-sent ]
-    then
-        rm ${REMOTE_DIR}/log/email-sent
-        echo "removed"
-    else
-        echo "no exists"
-    fi
+${SSH} /bin/bash << EOF
+	if [ -f ${REMOTE_DIR}/log/email-sent ]
+	then
+		rm ${REMOTE_DIR}/log/email-sent
+		echo "removed"
+	else
+		echo "no exists"
+	fi
 EOF
