20674bdb0303cc25ed9a99170be0f2f6095c7c62
Jakub Bouček <pan@jakubboucek.cz>
2019-04-25 10:20:51 +0200

    Deploy script - remove uneffective command

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,6 @@ SSH="ssh ${SERVER_NAME}"
 echo "Uploading data to SSH…"
 rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}${LOCAL_DIR}/" "${SERVER_NAME}:$REMOTE_DIR/"
 
-echo "Replace file permissions…"
-${SSH} sudo fixwww ${REMOTE_DIR}
-
 echo "Remove temporary files…"
 ${SSH} find ${REMOTE_DIR}/temp -mindepth 2 -type f -delete
 
