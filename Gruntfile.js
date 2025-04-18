b20c220642cf2b7b1dd6191770fc227dcf8ff732
Jakub Bouček <pan@jakubboucek.cz>
2025-04-18 22:59:14 +0200

    npm+grunt: upgrade deps, disable beautify in minifies files

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 sourceMap: false,
-                beautify: true
+                beautify: false
             },
             default: {
                 files: {
